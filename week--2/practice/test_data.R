38330e036a9896c4c7ce41d6298ca1f7f568c2d4
Zhiyuan3027 <b07505023@ntu.edu.tw>
2018-09-26 11:34:05 +0800

    926 change

--- week--2/practice/test_data.R
+++
@@ -1,9 +0,0 @@
-#prepare
-library(rvest)
-url="https://github.com/Zhiyuan3027/107-1RSampleCode/blob/master/week_2/course_2/test_data.csv#L11"
-
-song.name=read_html(url)%>%
-html_nodes("td , th")%>%
-html_text()
-song.name
-
